c906c84cc14e28e768537c6fba9a12690080b0a6
zacariasyang <zac.yang@gmail.com>
2016-03-15 12:05:19 +0800

    first commit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/methodfortheunknown/"
+http_images_path = "/methodfortheunknown/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/methodfortheunknown/fonts"
+css_dir = "public/methodfortheunknown/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
